8446d5b5478090f844a5fae1452e10edcb899334
Sean Cooper <seancooper@Seans-Air.lan>
2023-07-26 12:11:13 -0700

    fixed apostrophy build error

--- app/components/LizCard.tsx
+++ app/components/LizCard.tsx
@@ -6,7 +6,7 @@ function LizCard() {
     <div className="relative bg-liz-card bg-cover bg-no-repeat bg-center w-full h-full">
      <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 flex flex-col items-center justify-center text-white transition-opacity duration-300">
         <p className="text-xl font-semibold">Learn More</p>
-        <p className="text-sm">Click to explore Liz's Website</p>
+        <p className="text-sm">Click to explore Liz&apos;s Website</p>
       </div>
     </div>
     
--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
           <div className="flex flex-col w-full justify-center items-center p-8">
             <h1 className="text-3xl md:text-5xl p-2 text-gray-900 font-raleway leading-none">
-              Hi, I'm <span className="text-blue-500">Sean Cooper</span>,
+              Hi, I&apos;m <span className="text-blue-500">Sean Cooper</span>,
             </h1>
             <h2 className="text-3xl md:text-5xl leading-none font-raleway text-gray-800">
               a Frontend Engineer.
