52ab8f0083fd05db4b517a211fd6012bd5b50fcb
Sean Cooper <seancooperwd@gmail.com>
2023-07-26 14:59:17 -0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='/icon.png'/>
+        <link rel='icon' href='/favicon.ico'/>
       </head>
       <body className={inter.className}>
         <Header />
